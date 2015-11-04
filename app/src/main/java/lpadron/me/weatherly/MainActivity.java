e21ecaf26751b61997d0c10ba1cf0bb78f5d3b62
Luis Padron <luispadronedu@gmail.com>
2015-11-04 00:55:33 -0500

    Removed default methods. Added OkHttp dependencies

--- app/src/main/java/lpadron/me/weatherly/MainActivity.java
+++ app/src/main/java/lpadron/me/weatherly/MainActivity.java
@@ -5,48 +5,48 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        String apiKey = "a45f738558f376111212234d47a716f6";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String finalUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + ","
+                + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(finalUrl).build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+
+            if (response.isSuccessful()){
+                Log.v(TAG, response.body().string());
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "EXCEPTION CAUGHT:  ", e);
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
