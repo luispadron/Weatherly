db6282ac35b1a8268dcbd80cb70c9b22a998e18c
Luis Padron <luispadronedu@gmail.com>
2015-11-04 14:01:00 -0500

    Added Weather class: for storing JSON information from Forcast.io

---
+++ app/src/main/java/lpadron/me/weatherly/Weather.java
@@ -0,0 +1,58 @@
+package lpadron.me.weatherly;
+
+public class Weather {
+    private String icon;
+    private long time;
+    private Double temp;
+    private Double humidity;
+    private Double percip;
+    private String summary;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
+    public Double getPercip() {
+        return percip;
+    }
+
+    public void setPercip(Double percip) {
+        this.percip = percip;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
