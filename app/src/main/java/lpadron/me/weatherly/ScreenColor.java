dbfdca3cffc8938e4ef46f768cb2971f1895498f
Luis Padron <luispadronedu@gmail.com>
2015-11-08 02:06:15 -0500

    Added code into ScreenColor class that allows background color to change based on time of day.

--- app/src/main/java/lpadron/me/weatherly/ScreenColor.java
+++ app/src/main/java/lpadron/me/weatherly/ScreenColor.java
@@ -31,14 +31,38 @@ public class ScreenColor {
     }
 
     public int getCorrectColor() {
+        /* Get the correct date/time */
         Date datetime = new Date (this.time * 1000);
         Calendar cal = Calendar.getInstance();
         cal.setTimeZone(TimeZone.getTimeZone(timeZone));
         cal.setTime(datetime);
-
+        /* Figure out the correct background color based on time of day */
+        int color = R.color.defaultColor;
         int hour = cal.get(Calendar.HOUR_OF_DAY);
-        int result = R.color.defaultColor;
 
-        return result;
+          /* Hour is in military time */
+        if (hour == 0){
+            color = R.color.earlyEvening;
+        } else if (hour >= 1 && hour < 5){
+            //late evening is 1 am - 4 am
+            color = R.color.lateEvening;
+        } else if (hour >= 5 && hour < 9) {
+            //early morning from 5 am - 8 am
+            color = R.color.earlyMorning;
+        }else if (hour >= 9 && hour < 12) {
+            //late morning from 9 am - 11 am
+            color = R.color.lateMorning;
+        }else if (hour >= 12 && hour < 15) {
+            //early afternoon from 12 pm - 2 pm
+            color = R.color.earlyAfternoon;
+        }else if (hour >= 15 && hour < 18) {
+            //late afternoon from 3 pm - 5 pm
+            color = R.color.lateAfternoon;
+        }else {
+            //early evening from 6 pm - 11 pm
+            color = R.color.earlyEvening;
+        }
+
+        return color;
     }
 }
