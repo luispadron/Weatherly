85f75d1522f097831665d62f5e512ee85534c5ee
luispadron <luispadronedu@gmail.com>
2015-11-16 20:10:01 -0500

    Replaces first day of week with Today

--- app/src/main/java/lpadron/me/weatherly/adapters/DayAdapter.java
+++ app/src/main/java/lpadron/me/weatherly/adapters/DayAdapter.java
@@ -61,7 +61,11 @@ public class DayAdapter extends BaseAdapter {
 
         holder.iconImageView.setImageResource(day.getIconId());
         holder.tempLabel.setText(day.getTempHigh() + "");
-        holder.dayLabel.setText(day.getDayOfTheWeek());
+        if (position == 0) {
+            holder.dayLabel.setText("Today");
+        } else {
+            holder.dayLabel.setText(day.getDayOfTheWeek());
+        }
         return convertView;
     }
 
